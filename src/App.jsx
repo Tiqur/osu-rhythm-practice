e573ed427a36df3119681436ab93dba713224e9e
Tiqur <Tiqur7@gmail.com>
2021-02-26 18:07:31 -0800

    Background and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
+import FlexDiv from './components/FlexDiv';
+import Text from './components/Text';
+
 // Main page
+const BackgroundContainer = styled(FlexDiv)`
+  background-color: #192035;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
 
 function App() {
   return (
-    <p>Setup</p>
+    <BackgroundContainer>
+      <Text size={"50px"}>osu! Rhythm practice</Text>
+    </BackgroundContainer>
   );
 }
 
