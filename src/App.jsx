0529cc37df3e5e56babf46af1b9f52506fcf419f
Tiqur <Tiqur7@gmail.com>
2021-02-26 18:32:05 -0800

    Setup page layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,50 @@ import Text from './components/Text';
 // Main page
 const BackgroundContainer = styled(FlexDiv)`
   background-color: #192035;
-  justify-content: center;
+  flex-direction: column;
   width: 100%;
   height: 100vh;
 `;
+
+const NavDiv = styled(FlexDiv)`
+  justify-content: center;
+  align-items: center;
+  background-color: #192035;
+  width: 100%;
+  height: 60px;
+`;
+
+const FooterDiv = styled(FlexDiv)`
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  width: 100%;
+  height: 40px;
+`;
+
+const ContentContainer = styled(FlexDiv)`
+  background-color: #192035;
+  width: auto;
+  height: 100%;
+  margin: 0px 40px 0px 40px;
+`;
+
+
+
 
 
 function App() {
   return (
     <BackgroundContainer>
-      <Text size={"50px"}>osu! Rhythm practice</Text>
+      <NavDiv>
+        <Text size={"35px"}>osu! Rhythm practice</Text>
+      </NavDiv>
+      <ContentContainer>
+
+      </ContentContainer>
+      <FooterDiv>
+
+      </FooterDiv>
     </BackgroundContainer>
   );
 }
