34bfbfa2ac4e1864b01d24e2a333082395522b40
Tiqur <Tiqur7@gmail.com>
2021-02-26 20:22:33 -0800

    Content grid setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import FlexDiv from './components/FlexDiv';
 import Text from './components/Text';
+import Container from './components/Container';
 
 // Main page
 const BackgroundContainer = styled(FlexDiv)`
@@ -15,26 +16,43 @@ const NavDiv = styled(FlexDiv)`
   align-items: center;
   background-color: #192035;
   width: 100%;
-  height: 60px;
+  height: 80px;
+  margin-bottom: 40px;
 `;
 
 const FooterDiv = styled(FlexDiv)`
   justify-content: center;
   align-items: center;
-  background-color: red;
+  background-color: #192035;
   width: 100%;
   height: 40px;
 `;
 
-const ContentContainer = styled(FlexDiv)`
-  background-color: #192035;
+const ContentContainer = styled.div`
+  display: grid;
+  grid-area: 1/2/4/6;
   width: auto;
   height: 100%;
   margin: 0px 40px 0px 40px;
+  grid-template-areas: 
+  "o o o . . . . . . . . ."
+  "o o o . . . . . . . . ."
+  "o o o . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . ."
+  ". . . . . . . . . . . .";
 `;
 
 
-
+const Options = styled(Container)`
+  grid-area: o;
+`;
 
 
 function App() {
@@ -44,7 +62,7 @@ function App() {
         <Text size={"35px"}>osu! Rhythm practice</Text>
       </NavDiv>
       <ContentContainer>
-
+        <Options />
       </ContentContainer>
       <FooterDiv>
 
